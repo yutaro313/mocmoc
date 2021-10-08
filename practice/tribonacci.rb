60f85f4bfb28fff665b6fa7be914c48b5fb24361
yutaro313 <yuutarou55@icloud.com>
2021-10-08 12:21:59 +0000

    update

--- practice/tribonacci.rb
+++ practice/tribonacci.rb
@@ -4,16 +4,16 @@ ruby mocmoc/practice/tribonacci.rb
 a = 1
 b = 3
 c = 7
-n = 0
-while n < 47
-  d = a + b + c
-  a = b
-  b = c
-  c = d
-  n += 1
-end
+# n = 0
+# while n < 47
+#   d = a + b + c
+#   a = b
+#   b = c
+#   c = d
+#   n += 1
+# end
 
-puts c
+# puts c
 
 # 今回の問題
 # 1,3,7,11,21,39...
